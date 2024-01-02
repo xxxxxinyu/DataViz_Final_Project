a16d42d006a81ab13158cacaacb54f784f8221c7
Ninh Quach <103368165+gdncs09@users.noreply.github.com>
2024-01-02 09:23:53 +0800

    bar charrt

--- index.js
+++ index.js
@@ -25,13 +25,13 @@ const line = g.append("g");
 
 let topologyData;
 let economyData;
+let internetData;
 const idMapping = {};
 const data = {};
 
 // 更新地圖數據
-const updataData = (topology, economy, year) => {
+const updataData = (topology, economy, year, internet) => {
     const filteredData = economy.filter(d => +d[' Year '] === year);
-    
     filteredData.forEach(d => {
         const alpha3 = idMapping[d[' CountryID '].padStart(3, '0')];
         d.Year = +d[' Year '];
@@ -64,6 +64,11 @@ const updataData = (topology, economy, year) => {
             .style("stroke", "transparent")
     };
 
+    const countryInternet = internet.filter(item => item['Code'] === 'CAN');
+    const countryGNI = economy.filter(item => item['Code'] === 'CAN');
+    console.log(countryGNI)
+
+    updateGraph(countryInternet)
     poly.selectAll("path")
         .data(topojson.feature(topology, topology.objects.world_polygons_simplified).features)
         .join("path")
@@ -75,34 +80,220 @@ const updataData = (topology, economy, year) => {
         .on("mouseleave", mouseleave )
         .on("click", function(event, d) {
             // 建議用iso3去找對應的數據
-            console.log(d);
+            const countryInternet = internet.filter(item => item['Code'] === d.properties.iso3);
+            updateGraph(countryInternet)
         });
 
 };
 
-const dataURL = "Global Economy Indicators.csv";
+//小圖表
+const graphMargin = {top: 30, bottom: 50, left: 50, right: 50},
+    graphWidth = width/2,
+    graphHeight = height/2;
+const innerWidth = graphWidth - graphMargin.left - graphMargin.right,
+    innerHeight = graphHeight - graphMargin.top - graphMargin.bottom;
+const graphSvg = svg.append('g')
+    .attr('class', 'graph')
+    .attr('transform', `translate(${margin.left}, ${margin.top})`)
+    .style('display', 'none');
+
+const updateGraph = (countryInternet) => {
+    graphSvg.selectAll('*').remove();
+
+    const years = countryInternet.map(d => d.Year);
+    console.log(years)
+    if (countryInternet.length > 0){
+        console.log(countryInternet)
+        graphSvg.append("rect")
+            .attr("fill", "white")
+            .attr("stroke", "#aaa")
+            .attr("x", 0)
+            .attr("y", 0)
+            .attr("width", graphWidth)
+            .attr("height", graphHeight);
+
+        const charts = graphSvg.append('g')
+            .attr("width", innerWidth)
+            .attr("height", innerHeight)
+            .attr('transform', `translate(${graphMargin.left}, ${graphMargin.top})`);
+        
+        //X axis
+        const x = d3.scaleBand().range([0, innerWidth]).padding(0.5);
+        x.domain(years.map(String));
+        charts.append('g')
+            .attr('transform', `translate(0, ${innerHeight})`)
+            .call(d3.axisBottom(x))
+            .selectAll('text')
+            .attr('transform', 'rotate(-45)')
+            .style('text-anchor', 'end');
+        
+        //Y axis
+        /*
+        const y = d3.scaleLinear().range([innerHeight, 0]);
+        y.domain([0, 100]);
+
+        charts.append('g')
+            .call(d3.axisLeft(y))
+            .append('text')
+            .attr('fill', '#000')
+            .attr('x', 40)
+            .attr('y', -20)
+            .attr('dy', '0.71em')
+            .attr('text-anchor', 'end')
+            .text('Internet Users(%)');*/
+
+        const z = d3.scaleLinear().range([innerHeight, 0]);
+        z.domain([0, d3.max(countryInternet, d => d['No. of Internet Users'])]);
+            
+        charts.append('g')
+            .attr('transform', `translate(${innerWidth}, 0)`)
+            .call(d3.axisRight(z))
+            .append('text')
+            .attr('fill', '#000')
+            .attr('x', 40)
+            .attr('y', -20)
+            .attr('dy', '0.71em')
+            .attr('text-anchor', 'end')
+            .text('No. of Internet Users');
+        /*
+        const bars = charts.append('g')
+            .selectAll('.bar')
+            .data(countryInternet)
+            .enter().append('rect')
+            .attr('class', 'bar')
+            .attr('x', d => x(String(d.Year)))
+            .attr('y', d => y(d['Internet Users(%)']))
+            .attr('width', x.bandwidth())
+            .attr('height', d => innerHeight - y(d['Internet Users(%)']))
+            .attr('fill', 'steelblue');
+        */
+        // Lines
+        const updateGraph = (countryInternet) => {
+            graphSvg.selectAll('*').remove();
+        
+            const years = countryInternet.map(d => d.Year);
+            console.log(years)
+            if (countryInternet.length > 0){
+                console.log(countryInternet)
+                graphSvg.append("rect")
+                    .attr("fill", "white")
+                    .attr("stroke", "#aaa")
+                    .attr("x", 0)
+                    .attr("y", 0)
+                    .attr("width", graphWidth)
+                    .attr("height", graphHeight);
+        
+                const charts = graphSvg.append('g')
+                    .attr("width", innerWidth)
+                    .attr("height", innerHeight)
+                    .attr('transform', `translate(${graphMargin.left}, ${graphMargin.top})`);
+                
+                //X axis
+                const x = d3.scaleBand().range([0, innerWidth]).padding(0.5);
+                x.domain(years.map(String));
+                charts.append('g')
+                    .attr('transform', `translate(0, ${innerHeight})`)
+                    .call(d3.axisBottom(x))
+                    .selectAll('text')
+                    .attr('transform', 'rotate(-45)')
+                    .style('text-anchor', 'end');
+                
+                //Y axis
+                /*
+                const y = d3.scaleLinear().range([innerHeight, 0]);
+                y.domain([0, 100]);
+        
+                charts.append('g')
+                    .call(d3.axisLeft(y))
+                    .append('text')
+                    .attr('fill', '#000')
+                    .attr('x', 40)
+                    .attr('y', -20)
+                    .attr('dy', '0.71em')
+                    .attr('text-anchor', 'end')
+                    .text('Internet Users(%)');*/
+        
+                    const z = d3.scaleLinear().range([innerHeight, 0]);
+                    z.domain([0, d3.max(countryInternet, d => d['No. of Internet Users'])]);
+                    
+                charts.append('g')
+                    .attr('transform', `translate(${innerWidth}, 0)`)
+                    .call(d3.axisRight(z))
+                    .append('text')
+                    .attr('fill', '#000')
+                    .attr('x', 40)
+                    .attr('y', -20)
+                    .attr('dy', '0.71em')
+                    .attr('text-anchor', 'end')
+                    .text('No. of Internet Users');
+                /*
+                const bars = charts.append('g')
+                    .selectAll('.bar')
+                    .data(countryInternet)
+                    .enter().append('rect')
+                    .attr('class', 'bar')
+                    .attr('x', d => x(String(d.Year)))
+                    .attr('y', d => y(d['Internet Users(%)']))
+                    .attr('width', x.bandwidth())
+                    .attr('height', d => innerHeight - y(d['Internet Users(%)']))
+                    .attr('fill', 'steelblue');
+                */
+                // Lines
+            const line = d3.line()
+                .x(d => x(String(d.Year)) + x.bandwidth() / 2)
+                .y(d => z(d['No. of Internet Users']));
+    
+            const lines = charts.append('path')
+                .datum(countryInternet)
+                .attr('class', 'line')
+                .attr('d', line)
+                .attr('stroke', 'red');
+        
+                graphSvg.style('display', 'block');
+                
+                
+            }
+            else{
+                graphSvg.style('display', 'none');
+            };
+        };
+
+        graphSvg.style('display', 'block');
+        
+        
+    }
+    else{
+        graphSvg.style('display', 'none');
+    };
+};
+//------------------------------//
+
+const dataURL = "Global Economy Indicators.csv"; 
 const polygonsURL = "https://raw.githubusercontent.com/GDS-ODSSS/unhcr-dataviz-platform/master/data/geospatial/world_polygons_simplified.json";
 const polylinesURL = "https://raw.githubusercontent.com/GDS-ODSSS/unhcr-dataviz-platform/master/data/geospatial/world_lines_simplified.json";
 const isoURL = "https://raw.githubusercontent.com/lukes/ISO-3166-Countries-with-Regional-Codes/master/all/all.csv";
 
+const data1URL = "Global Internet users.csv";
+
 const promises = [
     d3.json(polygonsURL),
     d3.csv(dataURL),
-    d3.csv(isoURL)
+    d3.csv(isoURL),
+    d3.csv(data1URL)
 ];
 
 // 載入數據並建立country code和alpha3的對應關係
 Promise.all(promises).then(ready);
-function ready([topology, economy, iso]) {
+function ready([topology, economy, iso, internet]) {
     const defaultYear = 1980;
     topologyData = topology;
     economyData = economy;
-    
+    internetData = internet;
     iso.forEach(entry => {
         idMapping[entry['country-code']] = entry['alpha-3'];
     });
     
-    updataData(topologyData,  economyData, defaultYear);
+    updataData(topologyData,  economyData, defaultYear, internetData);
 
 };
 
@@ -154,7 +345,7 @@ var slider = d3.sliderVertical()
     .height(400)
     .tickFormat(d3.format('d'))
     .on('onchange', val => {
-        updataData(topologyData, economyData, val);   
+        updataData(topologyData, economyData, val, internetData);   
     }); 
 
 g.append('g')
